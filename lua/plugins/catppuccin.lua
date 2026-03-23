fe3e6bd8ab93b11e9074308b72bed7c91767f9a1
fabiooo4 <fabioirimie@gmail.com>
2026-03-23 18:58:57 +0100

    feat: added transparent background for catppuccin colorscheme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,4 +2,8 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
+  opts = {
+    -- Remove transparent mode if running neovide
+    transparent_background = not vim.g.neovide
+  }
 }
