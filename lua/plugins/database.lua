febcdaf6dfc8c8e74ab227df8c66cfc43734335f
fabiooo4 <fabioirimie@gmail.com>
2026-03-24 10:48:31 +0100

    feat: added custom Columns helper for DBUI

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -1,24 +1,32 @@
 return {
-  'kristijanhusak/vim-dadbod-ui',
-  dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
-  },
-  cmd = {
-    'DBUI',
-    'DBUIToggle',
-    'DBUIAddConnection',
-    'DBUIFindBuffer',
-  },
-  init = function()
-    vim.g.db_ui_use_nerd_fonts = 1
+	"kristijanhusak/vim-dadbod-ui",
+	dependencies = {
+		{ "tpope/vim-dadbod", lazy = true },
+		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+	},
+	cmd = {
+		"DBUI",
+		"DBUIToggle",
+		"DBUIAddConnection",
+		"DBUIFindBuffer",
+	},
+	init = function()
+		vim.g.db_ui_use_nerd_fonts = 1
 
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = { 'sql', 'dbui' },
-      callback = function()
-        local opts = { noremap = true, silent = true }
-        vim.api.nvim_buf_set_keymap(0, 'n', '<leader>d', ':DBUIToggle<CR>', opts)
-      end,
-    })
-  end,
+		-- Keybinds
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "sql", "dbui" },
+			callback = function()
+				local opts = { noremap = true, silent = true }
+				vim.api.nvim_buf_set_keymap(0, "n", "<leader>d", ":DBUIToggle<CR>", opts)
+			end,
+		})
+
+		-- Custom helpers
+		vim.g.db_ui_table_helpers = {
+			postgresql = {
+				Columns = "select column_name as attribute, data_type, is_nullable, column_default as default, character_maximum_length as charmax, is_generated, is_updatable from information_schema.columns where table_name='{table}' and table_schema='public' order by ordinal_position",
+			},
+		}
+	end,
 }
